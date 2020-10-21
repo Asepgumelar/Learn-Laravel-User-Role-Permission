e96a86400038e32c20b6f66d2f8481237d1dcaa6
Asep Gumelar <48475205+Asepgumelar@users.noreply.github.com>
2020-10-21 16:59:10 +0700

    add controller for product

---
+++ app/Http/Controllers/Backend/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function showCreate()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function delete()
+    {
+        //
+    }
+}
